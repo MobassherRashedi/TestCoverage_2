5ee99c19bd958c03ea9723b107ca5bfe4940b4da
MD Mobassher Rashedi <cse.rashedi@gmail.com>
2025-01-26 18:38:15 +0600

    Initial project setup with API and tests

---
+++ CodeCoverage/Services/ICalculatorService.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeCoverage.Services
+{
+    public interface ICalculatorService
+    {
+        int Add(int a, int b);
+        int Substract(int a, int b);
+        int Multiplication(int a, int b);
+        int Division(int a, int b);
+    }
+}
